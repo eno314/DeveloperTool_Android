1001213309168a58c9114ec8819fa6e1439a5373
Hiroto Kitamura <kirikonknow@gmail.com>
2018-09-12 20:07:07 +0900

    first commit

---
+++ app/src/main/java/jp/eno314/developertool/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.eno314.developertool
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
