52a7a06ca7018d520f009c7213c447862e0aaca0
Hiroto Kitamura <kirikonknow@gmail.com>
2018-09-12 21:01:07 +0900

    色を変えるところまで実装

--- app/src/main/java/jp/eno314/developertool/MainActivity.kt
+++ app/src/main/java/jp/eno314/developertool/MainActivity.kt
@@ -1,12 +1,23 @@
 package jp.eno314.developertool
 
+import android.databinding.DataBindingUtil
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import jp.eno314.developertool.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.button.setOnClickListener({
+            val inputText = binding.textInputLayout.editText?.text?.toString()
+                    ?: return@setOnClickListener
+            binding.root.setBackgroundColor(Color.parseColor(inputText))
+        })
     }
 }
